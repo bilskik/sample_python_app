c0a4ff91e43e7454b04e05612392e2c2cee654ba
Kamil Bilski <97744349+bilskik@users.noreply.github.com>
2024-11-09 18:18:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ db.create_all()
 def test():
   return make_response(jsonify({'message': 'test route'}), 200)
 
-@app.route('/flag', methods=['GET'])
+@app.route('/health-test', methods=['GET'])
 def flag():
   flag = environ.get("CTF_FLAG")
-  return make_response(jsonify({'message': flag}), 200)
+  return make_response(jsonify({'flag': flag, 'db' : db}), 200)
 
 # create a user
 @app.route('/users', methods=['POST'])
